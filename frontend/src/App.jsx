bb41e420e04e419bdbcba69458881f916b3a91a0
mavish12 <slodh079@gmail.com>
2025-02-23 21:16:28 +0530

    Used Outlet to render the Layouts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<UserLayout/>}>{/* User Layout */}</Route>
+        <Route path="/" element={<UserLayout/>}>
+          <Route index element={<Home/>}/>
+        </Route>
         <Route>{/* Admin Layout */}</Route>
       </Routes>
     </BrowserRouter>
--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Common/Header'
 import Footer from '../Common/Footer'
+import { Outlet } from 'react-router-dom'
 
 
 const UserLayout = () => {
@@ -9,6 +10,9 @@ const UserLayout = () => {
         {/* Header */}
         <Header/>
         {/* Main Content */}
+        <main>
+          <Outlet/>
+        </main>
         {/* Footer */}
         <Footer/>
     </>
