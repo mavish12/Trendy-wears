42e144085aaa70af6daa76de9a6fc2ded7023dd3
mavish12 <slodh079@gmail.com>
2025-02-27 00:18:40 +0530

    Login and Register UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
 import { Toaster } from "sonner"
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<UserLayout/>}>
           <Route index element={<Home/>}/>
+          <Route path="login" element={<Login/>}/>  
+          <Route path="register" element={<Register/>}/>  
         </Route>
         <Route>{/* Admin Layout */}</Route>
       </Routes>
