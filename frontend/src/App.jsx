51af597d2aea65cb3edd98c63df856322a98c4b2
mavish12 <slodh079@gmail.com>
2025-02-28 02:58:20 +0530

    CollectionPage and Filter Sidebar started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { Toaster } from "sonner"
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import CollectionPage from "./pages/CollectionPage";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="login" element={<Login/>}/>  
           <Route path="register" element={<Register/>}/>
           <Route path="profile" element={<Profile/>}/>
+          <Route path="collections/:collection" element={<CollectionPage/>} />
         </Route>
         <Route>{/* Admin Layout */}</Route>
       </Routes>
---
+++ frontend/src/pages/CollectionPage.jsx
@@ -0,0 +1,188 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { FaFilter } from "react-icons/fa";
+import FilterSidebar from '../components/Products/FilterSidebar';
+
+const CollectionPage = () => {
+
+    const [products, setProducts] = useState([])
+    const sidebarRef = useRef(null)
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+    const toggleSidebar = () =>{
+        setIsSidebarOpen(!isSidebarOpen)
+    }
+
+    const handleClickOutside = (e) =>{
+        if(sidebarRef.current && !sidebarRef.current.contains(e.target)){
+            setIsSidebarOpen(false)
+        }
+    }
+
+    useEffect(() =>{
+        // Event listener for closing sidebar even when clicked anywhere outside
+        document.addEventListener("mousedown", handleClickOutside)
+        //Clean event listner
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+            }
+    })
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            const fetchedProducts = [
+                {
+                  _id: 1,
+                  name: "Stylish T-Shirt",
+                  price: 80,
+                  // originalPrice: 100,
+                  // description: "A stylish t-shirt for men in casual purpose.",
+                  // brand: "ZARA",
+                  // material: "Cotton",
+                  // sizes: ["S", "M", "L", "XL"],
+                  // colors: ["Black", "White", "Red"],
+                  images: [
+                    {
+                      url: "https://picsum.photos/500/500?random=11",
+                      altText: "Stylish T-Shirt 1",
+                    },
+                  ],
+                },
+                {
+                  _id: 2,
+                  name: "Stylish Jacket",
+                  price: 180,
+                  // originalPrice: 100,
+                  // description: "A stylish t-shirt for men in casual purpose.",
+                  // brand: "ZARA",
+                  // material: "Cotton",
+                  // sizes: ["S", "M", "L", "XL"],
+                  // colors: ["Black", "White", "Red"],
+                  images: [
+                    {
+                      url: "https://picsum.photos/500/500?random=12",
+                      altText: "Stylish Jacket",
+                    },
+                  ],
+                },
+                {
+                  _id: 3,
+                  name: "Party Blazer",
+                  price: 210,
+                  // originalPrice: 100,
+                  // description: "A stylish t-shirt for men in casual purpose.",
+                  // brand: "ZARA",
+                  // material: "Cotton",
+                  // sizes: ["S", "M", "L", "XL"],
+                  // colors: ["Black", "White", "Red"],
+                  images: [
+                    {
+                      url: "https://picsum.photos/500/500?random=13",
+                      altText: "Party Blazer",
+                    },
+                  ],
+                },
+                {
+                  _id: 4,
+                  name: "Beach Shirt",
+                  price: 50,
+                  // originalPrice: 100,
+                  // description: "A stylish t-shirt for men in casual purpose.",
+                  // brand: "ZARA",
+                  // material: "Cotton",
+                  // sizes: ["S", "M", "L", "XL"],
+                  // colors: ["Black", "White", "Red"],
+                  images: [
+                    {
+                      url: "https://picsum.photos/500/500?random=14",
+                      altText: "Beach Shirt",
+                    },
+                  ],
+                },
+                {
+                  _id: 5,
+                  name: "Beach Shirt",
+                  price: 50,
+                  // originalPrice: 100,
+                  // description: "A stylish t-shirt for men in casual purpose.",
+                  // brand: "ZARA",
+                  // material: "Cotton",
+                  // sizes: ["S", "M", "L", "XL"],
+                  // colors: ["Black", "White", "Red"],
+                  images: [
+                    {
+                      url: "https://picsum.photos/500/500?random=7",
+                      altText: "Beach Shirt",
+                    },
+                  ],
+                },
+                {
+                  _id: 6,
+                  name: "Beach Shirt",
+                  price: 50,
+                  // originalPrice: 100,
+                  // description: "A stylish t-shirt for men in casual purpose.",
+                  // brand: "ZARA",
+                  // material: "Cotton",
+                  // sizes: ["S", "M", "L", "XL"],
+                  // colors: ["Black", "White", "Red"],
+                  images: [
+                    {
+                      url: "https://picsum.photos/500/500?random=8",
+                      altText: "Beach Shirt",
+                    },
+                  ],
+                },
+                {
+                  _id: 7,
+                  name: "Beach Shirt",
+                  price: 50,
+                  // originalPrice: 100,
+                  // description: "A stylish t-shirt for men in casual purpose.",
+                  // brand: "ZARA",
+                  // material: "Cotton",
+                  // sizes: ["S", "M", "L", "XL"],
+                  // colors: ["Black", "White", "Red"],
+                  images: [
+                    {
+                      url: "https://picsum.photos/500/500?random=9",
+                      altText: "Beach Shirt",
+                    },
+                  ],
+                },
+                {
+                  _id: 8,
+                  name: "Beach Shirt",
+                  price: 50,
+                  // originalPrice: 100,
+                  // description: "A stylish t-shirt for men in casual purpose.",
+                  // brand: "ZARA",
+                  // material: "Cotton",
+                  // sizes: ["S", "M", "L", "XL"],
+                  // colors: ["Black", "White", "Red"],
+                  images: [
+                    {
+                      url: "https://picsum.photos/500/500?random=10",
+                      altText: "Beach Shirt",
+                    },
+                  ],
+                },
+              ];
+        setProducts(fetchedProducts)
+        }, 1000)
+    }, [])
+
+  return (
+    <div className='flex flex-col lg:flex-row'>
+        {/* Mobile Filter button */}
+        <button onClick={toggleSidebar} className='lg:hidden border p-2 flex justify-center items-center'>
+        <FaFilter className='mr-2'/> Filter
+        </button>
+        {/* Filter sidebar */}
+        <div ref={sidebarRef} className={`${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}`}>
+            <FilterSidebar />
+        </div>
+    </div>
+  )
+}
+
+export default CollectionPage
