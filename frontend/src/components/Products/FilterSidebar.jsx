a1d6ad2963361e76c4314569aa4a9b4ddebbf05f
mavish12 <slodh079@gmail.com>
2025-03-02 02:41:54 +0530

    Filtering Products Logic

--- frontend/src/components/Products/FilterSidebar.jsx
+++ frontend/src/components/Products/FilterSidebar.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const FilterSidebar = () => {
   //To read and modify the query parameters
   //any.com/?a=1&b=2
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate()
   const [filters, setFilters] = useState({
     category: "",
     gender: "",
-    color: "",
+    color: [],
     size: [],
     material: [],
     brand: [],
@@ -67,15 +68,53 @@ const FilterSidebar = () => {
     setFilters({
       category: params.category || "",
       gender: params.gender || "",
-      color: params.color || "",
+      color: params.color ? params.color.split(",") : [],
       size: params.size ? params.size.split(",") : [],
       material: params.material ? params.material.split(",") : [],
       brand: params.brand ? params.brand.split(",") : [],
       minPrice: params.minPrice || 0,
-      maxPrice: params.maxPrice || 0,
+      maxPrice: params.maxPrice || 100,
     });
     setPriceRange([0, params.maxPrice || 100]);
   }, [searchParams]);
+
+  const handleFilterChange = (e, type = null) => {
+    let { name, value, checked } = e.target;
+    let newFilters = { ...filters };
+  
+    if (type === "color") {
+      // Handle color button clicks (toggle selection)
+      newFilters[name] = newFilters[name].includes(value)
+        ? newFilters[name].filter((item) => item !== value) // Remove if already selected
+        : [...newFilters[name], value]; // Add if not selected
+    } else if (e.target.type === "checkbox") {
+      // Handle checkbox selections
+      newFilters[name] = checked
+        ? [...(newFilters[name] || []), value]
+        : newFilters[name].filter((item) => item !== value);
+    } else {
+      // Handle radio buttons and other inputs
+      newFilters[name] = value;
+    }
+  
+    setFilters(newFilters);
+    updateUrlParams(newFilters)
+    console.log(newFilters);
+  };
+  
+  const updateUrlParams = (newFilters) =>{
+    const params = new URLSearchParams();
+    //{category: "Top Wear", size: ["XS","L"]}
+    Object.keys(newFilters).forEach((key)=> {
+        if(Array.isArray(newFilters[key]) && newFilters[key].length > 0){
+            params.append(key, newFilters[key].join(",")) // "XS,L"
+        }else if(newFilters[key]){
+            params.append(key, newFilters[key]) // "Top Wear"   
+        }
+    })
+    setSearchParams(params)
+    navigate(`?${params.toString()}`) //?category=Bottom+Wear&size=XS%2CS
+  }
 
   return (
     <div className="p-4">
@@ -88,6 +127,9 @@ const FilterSidebar = () => {
             <input
               type="radio"
               name="category"
+              value={category}
+              checked={filters.category === category}
+              onChange={handleFilterChange}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
             />
             <span className="text-gray-700">{category}</span>
@@ -102,6 +144,8 @@ const FilterSidebar = () => {
             <input
               type="radio"
               name="gender"
+              value={gender}
+              onChange={handleFilterChange}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
             />
             <span className="text-gray-700">{gender}</span>
@@ -115,7 +159,12 @@ const FilterSidebar = () => {
           {colors.map((color) => (
             <button
               key={color}
-              className="border border-gray-200 rounded-full w-8 h-8 cursor-pointer transition hover:scale-105"
+              name="color"
+              value={color}
+              onClick={(e) => handleFilterChange(e, "color")}
+              className={`border border-gray-300 rounded-full w-8 h-8 cursor-pointer transition hover:scale-105 ${
+                filters.color.includes(color) ? "ring-2 ring-blue-500" : ""
+              }`}
               style={{ backgroundColor: color.toLowerCase() }}
             ></button>
           ))}
@@ -130,6 +179,9 @@ const FilterSidebar = () => {
             <input
               type="checkbox"
               name="size"
+              value={size}
+              checked={filters.size === size}
+              onChange={handleFilterChange}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-500 border-gray-300"
             />
             <span className="text-gray-700">{size}</span>
@@ -145,6 +197,9 @@ const FilterSidebar = () => {
             <input
               type="checkbox"
               name="material"
+              value={material}
+              checked={filters.material === material}
+              onChange={handleFilterChange}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-500 border-gray-300"
             />
             <span className="text-gray-700">{material}</span>
@@ -160,6 +215,9 @@ const FilterSidebar = () => {
             <input
               type="checkbox"
               name="brand"
+              value={brand}
+              checked={filters.brand === brand}
+              onChange={handleFilterChange}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-500 border-gray-300"
             />
             <span className="text-gray-700">{brand}</span>
