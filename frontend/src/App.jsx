43b70c0558c3155034aff73d402bdf9a4a837697
mavish12 <slodh079@gmail.com>
2025-03-08 17:53:22 +0530

    Order Confirmation Page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import CollectionPage from "./pages/CollectionPage";
 import ProductDetails from "./components/Products/ProductDetails";
 import CartPage from "./pages/CartPage";
 import Checkout from "./components/Cart/Checkout";
+import OrderConfirmationPage from "./pages/OrderConfirmationPage";
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route path="collections/:collection" element={<CollectionPage/>} />
           <Route path="product/:id" element={<ProductDetails/>}/>
           <Route path="checkout" element={<Checkout/>}/>
+          <Route path="order-confirmation" element={<OrderConfirmationPage/>}/>
         </Route>
         <Route>{/* Admin Layout */}</Route>
       </Routes>
