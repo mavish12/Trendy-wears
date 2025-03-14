4a7aa7cc8363440286aba7f886c4a58e1fa003a8
mavish12 <slodh079@gmail.com>
2025-03-14 20:55:46 +0530

    Jwt implemented and Register route completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,20 +1,42 @@
 const express = require("express");
-const User = require("../models/User")
-const jwt = require("jsonwebtoken")
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
-
 // Route for regestering a new user(POST)
-router.post("/register", async(req, res) =>{
-    const {name, email, password} = req.body;
-    
-    try{
-        const user = await User.create({name, email, password});
-        res.status(201).json({message: "User created successfully", user});
-    }catch(err){
-        res.status(400).json({message: "Error creating user", err});
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already registered" });
     }
-})
+    // const user = await User.create({name, email, password});
+    user = new User({ name, email, password });
+    await user.save();
+    // Create JWT Payload
+    const payload = {
+      user: {
+        id: user._id,
+        role: user.role,
+      },
+    };
+    // Sign and return the token along with the user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" },
+      (err, token) => {
+        if (err) throw err;
+        // Send the user and token in response
+        res.status(201).json({ message: "User created successfully", user, token });
+      }
+    );
+  } catch (err) {
+    res.status(400).json({ message: "Error creating user", err });
+  }
+});
 
 module.exports = router;
